0319e4310eaf2b7eb89c4e8d320e5164efb8e647
Eric Hendrix <agentmrbig@gmail.com>
2019-07-25 21:31:19 -0400

    added update endpoint, added checks for name and bio on adding users and editing users

--- index.js
+++ index.js
@@ -48,17 +48,53 @@ server.get('/api/users/:id', (req, res) => {
 });
 
 server.post('/api/users', (req, res) => {
+    const { name, bio } = req.body;
 
-    Users.insert(req.body)
-        .then(user => {
-            res.status(201).json(user);
-        })
-        .catch(() => {
-            res.status(500).json({
-                errorMessage:
-                    'User not saved to database, there was an error',
+    if (!name || !bio) {
+        res
+            .status(400)
+            .json({ errorMessage: 'Name and bio is manditory for adding new users.' });
+    } else {
+        Users.insert(req.body)
+            .then(user => {
+                res.status(201).json(user);
+            })
+            .catch(() => {
+                res.status(500).json({
+                    errorMessage:
+                        'User not saved to database, there was an error',
+                });
             });
-        });
+    }
+
+});
+
+server.put('/api/users/:id', (req, res) => {
+    const { name, bio } = req.body;
+
+    if (!name || !bio) {
+        res
+            .status(400)
+            .json({ errorMessage: 'Name and bio is manditory for editing users.' });
+    } else {
+        Users.update(req.params.id, req.body)
+            .then(user => {
+                if (user) {
+                    res.status(200).json(user);
+                } else {
+                    res
+                        .status(404)
+                        .json({
+                            message: 'The user with the specified ID does not exist.',
+                        });
+                }
+            })
+            .catch(() => {
+                res.status(500).json({
+                    errorMessage: 'The user information could not be modified.',
+                });
+            });
+    }
 });
 
 
